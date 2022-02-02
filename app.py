e51fbfccaf76e8dd572d21290bc665cd4116ea32
Jillani Soft Tech <76724558+MGJillaniMughal@users.noreply.github.com>
2022-02-02 09:22:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,33 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+# Link with Bootstrap for navigation
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+    <a class="navbar-brand"></a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/MGJillaniMughal" target="_blank">Github</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/jillani-softtech/" target="_blank">LinkedIn</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.kaggle.com/jillanisofttech" target="_blank">Kaggle</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+""", unsafe_allow_html=True)
+
+
+
 st.markdown('''# **Whatsapp Chat Analyzer**
 Whatsapp Chat Analyzer By Jillani Soft Tech 😎.
 ''')
